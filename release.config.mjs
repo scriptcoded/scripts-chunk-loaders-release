7bb16ab0b248f9a0ae1b017ffb0c502bc01b9f81
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2025-08-02 16:57:15 +0200

    fix: correct title

--- release.config.mjs
+++ release.config.mjs
@@ -44,7 +44,7 @@ const config = {
           { path: "'build/libs/!(*-sources).jar'", label: "Mod file" },
           { path: "build/libs/*-sources.jar", label: "Development sources" },
         ],
-        releaseNameTemplate: "${nextRelease.version} - <%= process.env.MINECRAFT_VERSION %>",
+        releaseNameTemplate: "${nextRelease.version} for Minecraft <%= process.env.MINECRAFT_VERSION %>",
       },
     ],
   ],
